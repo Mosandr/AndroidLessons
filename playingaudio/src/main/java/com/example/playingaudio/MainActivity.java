a77399840aef99edffd6f188b069f5be5cbb174c
Andriy Mosol <mosandr5@gmail.com>
2020-03-31 13:47:54 +0300

    second commit

--- playingaudio/src/main/java/com/example/playingaudio/MainActivity.java
+++ playingaudio/src/main/java/com/example/playingaudio/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.playingaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mp;
+    SeekBar voleumSeekBar;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        MediaPlayer mp = MediaPlayer.create(this, R.raw.original);
          mp = MediaPlayer.create(getApplicationContext(), R.raw.original);
+
+         audioManager= (AudioManager) getSystemService(AUDIO_SERVICE);
+         int maxVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+         Log.d("maxVol", ""+maxVol);
+        voleumSeekBar = findViewById(R.id.seekBar);
+        voleumSeekBar.setMax(maxVol);
+        voleumSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+             @Override
+             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                 Log.d("progress",""+ progress);
+                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+             }
+
+             @Override
+             public void onStartTrackingTouch(SeekBar seekBar) {
+
+             }
+
+             @Override
+             public void onStopTrackingTouch(SeekBar seekBar) {
+
+             }
+         });
     }
 
     public void play(View view) {

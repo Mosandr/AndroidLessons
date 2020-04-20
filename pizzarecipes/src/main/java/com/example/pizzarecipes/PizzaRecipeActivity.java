729aca3a1c2f9502f7b980e5c0fba80ba6f95831
Andriy Mosol <mosandr5@gmail.com>
2020-04-20 23:46:45 +0300

    Commit 20 04

---
+++ pizzarecipes/src/main/java/com/example/pizzarecipes/PizzaRecipeActivity.java
@@ -0,0 +1,14 @@
+package com.example.pizzarecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PizzaRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_recipe);
+    }
+}

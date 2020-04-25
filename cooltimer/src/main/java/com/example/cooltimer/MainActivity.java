dbf1450a751a0adc0a727801d4e7b10d6636f98d
Andriy Mosol <mosandr5@gmail.com>
2020-04-25 18:30:56 +0300

    With Timer

---
+++ cooltimer/src/main/java/com/example/cooltimer/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.cooltimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+    MediaPlayer mp2;
+    SeekBar seekBar;
+    Button btn;
+    TextView textView;
+    int defaultProgress =30;
+    CountDownTimer myTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView =findViewById(R.id.textView);
+        seekBar=findViewById(R.id.seekBar);
+        btn=findViewById(R.id.btn);
+        mp = MediaPlayer.create(this,R.raw.bellsound);
+        mp2 = MediaPlayer.create(this,R.raw.tick_sound);
+
+        seekBar.setMax(600);
+        seekBar.setProgress(defaultProgress);
+        textView.setText(getTimeText(defaultProgress));
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(getTimeText(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+
+    public void onStartClick(View view) {
+        if(btn.getText().equals("START")){
+            btn.setText("STOP");
+            setMyTimer(seekBar.getProgress());
+            myTimer.start();
+            seekBar.setEnabled(false);
+
+        } else {
+            btn.setText("START");
+            seekBar.setEnabled(true);
+            myTimer.cancel();
+            seekBar.setProgress(defaultProgress);
+            textView.setText(getTimeText(defaultProgress));
+
+        }
+    }
+
+    public void setMyTimer(int timerTimeSec){
+        myTimer = new CountDownTimer(timerTimeSec*1000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                textView.setText(getTimeText((int) millisUntilFinished/1000));
+                mp2.start();
+            }
+
+            @Override
+            public void onFinish() {
+                mp.start();
+                seekBar.setProgress(defaultProgress);
+                btn.setText("START");
+            }
+        };
+    }
+
+    public String getTimeText(int seconds){
+        String timeText ="";
+        if(seconds/60<10){
+        timeText=timeText+"0"+seconds/60+":";}
+        else{timeText=timeText+seconds/60+":";}
+        if (seconds%60<10) {
+            timeText=timeText + "0" + seconds%60;
+        } else {timeText=timeText + seconds%60;}
+        return timeText;
+    }
+}
--- timerusing/src/main/java/com/example/timerusing/MainActivity.java
+++ timerusing/src/main/java/com/example/timerusing/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.timerusing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView=findViewById(R.id.textView1);
+
+        /**First option how to set timer*/
+//        final Handler handler = new Handler();
+//
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                TextView textView = findViewById(R.id.textView1);
+//                textView.append("\nTwo seconds are passed");
+//                handler.postDelayed(this, 2000);
+//            }
+//        };
+//        handler.post(runnable);
+
+        /** Second option how to set timer*/
+
+        CountDownTimer myTimer = new CountDownTimer(10000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                textView.setText(""+millisUntilFinished/1000+" seconds left");
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                textView.setText("Finish!");
+            }
+        };
     }
 }

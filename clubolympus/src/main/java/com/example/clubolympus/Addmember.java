192f8f0aaad63ae44c019029512d7c259f992520
Andriy Mosol <mosandr5@gmail.com>
2020-08-15 18:28:04 +0300

    Start Olimpus app

--- clubolympus/src/main/java/com/example/clubolympus/Addmember.java
+++ clubolympus/src/main/java/com/example/clubolympus/Addmember.java
@@ -1,8 +1,15 @@
 package com.example.clubolympus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -18,6 +25,26 @@ public class Addmember extends AppCompatActivity {
     private ArrayAdapter<CharSequence> spinnerAdapter;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_member_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.save_member:
+                return true;
+            case R.id.delete_member:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addmember);
@@ -31,5 +58,30 @@ public class Addmember extends AppCompatActivity {
                 this, R.array.array_gender, android.R.layout.simple_spinner_item);
          spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
          genderSpinner.setAdapter(spinnerAdapter);
+
+         genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+             @Override
+             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                 String selectedGender = (String) parent.getItemAtPosition(position);
+                 if (!TextUtils.isEmpty((selectedGender))){
+                     if (selectedGender.equals("Male")){
+                         gender =1;
+                     } else if (selectedGender.equals("Female")) {
+                         gender =2;
+                     } else {
+                         gender =0;
+                     }
+                 }
+             }
+
+             @Override
+             public void onNothingSelected(AdapterView<?> parent) {
+                 gender =0;
+             }
+         });
+         
+
+
+
     }
 }
